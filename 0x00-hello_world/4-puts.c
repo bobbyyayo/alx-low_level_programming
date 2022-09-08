ee5ee135dff6cb3849779d0e073ffae3ed1dd85e
bobbyyayo <deji.ajumobi04@gmail.com>
2022-09-08 16:50:32 -0700

    task 4 hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: Write a C program that prints a sentence.
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
