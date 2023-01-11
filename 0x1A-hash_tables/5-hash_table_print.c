816af05662d639da013d2f6aea8630a712a35900
bobbyyayo <deji.ajumobi04@gmail.com>
2023-01-11 12:54:40 -0800

    a function that prints a hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
- * 
+ *
  * Description: Key/value pairs are printed in the order
  * they appear in the array of the hash table.
  */
